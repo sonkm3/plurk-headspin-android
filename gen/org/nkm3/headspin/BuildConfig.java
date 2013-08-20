f3becb100d18c80b737b5838aeae601a0d94d538
sonkm3 <sonkm3@gmail.com>
2013-08-20 23:59:50 +0900

    remove files.

--- gen/org/nkm3/headspin/BuildConfig.java
+++
@@ -1,8 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-/** Automatically generated file. DO NOT MODIFY */
-package org.nkm3.headspin;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
